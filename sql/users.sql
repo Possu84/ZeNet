5f600a84a15ebb61d903c3ee2a98fdea1783dfb7
jonas pooters <pooters.jonas@gmail.com>
2018-09-06 15:09:37 +0200

    all login and registeration methods go to the app page. Next store all nessessary data to a cookie and serv it when we need info about the user

--- sql/users.sql
+++ sql/users.sql
@@ -5,5 +5,6 @@ CREATE TABLE users (
         first_name VARCHAR(255),
         last_name  VARCHAR(255),
         email      VARCHAR(255)NOT NULL UNIQUE,
-        password   VARCHAR(255)
+        password   VARCHAR(255),
+        picUrl     VARCHAR(300)
 );
